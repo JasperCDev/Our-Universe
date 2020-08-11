e5c6b140113f2e4007652a93fb09039371ca1656
JasperC1999 <jasperischauvin@gmail.com>
2020-08-11 17:56:20 -0500

    removed unecessary bots

--- back-end/server/index.ts
+++ back-end/server/index.ts
@@ -14,55 +14,55 @@ setInterval(() => {
   updateGlobalClicks({ body: { clicks: 35 } } as Request, { send: () => { } } as Response);
 }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 33, user_name: 'JosephBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 33 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 33, user_name: 'JosephBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 33 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 30, user_name: 'BethanyBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 30 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 30, user_name: 'BethanyBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 30 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 27, user_name: 'DavidBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 27 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 27, user_name: 'DavidBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 27 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 24, user_name: 'KellieBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 24 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 24, user_name: 'KellieBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 24 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 20, user_name: 'JayBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 20 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 20, user_name: 'JayBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 20 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 18, user_name: 'TimBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 18 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 18, user_name: 'TimBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 18 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 16, user_name: 'JimenaBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 16 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 16, user_name: 'JimenaBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 16 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 14, user_name: 'MarkBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 14 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 14, user_name: 'MarkBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 14 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 10, user_name: 'AbbieBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 10 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 10, user_name: 'AbbieBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 10 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
-setInterval(() => {
-  updateUserClicks({ body: { clicks: 7, user_name: 'RileyBOT' } } as Request, { send: () => { } } as Response);
-  updateGlobalClicks({ body: { clicks: 7 } } as Request, { send: () => { } } as Response);
-}, 2000);
+// setInterval(() => {
+//   updateUserClicks({ body: { clicks: 7, user_name: 'RileyBOT' } } as Request, { send: () => { } } as Response);
+//   updateGlobalClicks({ body: { clicks: 7 } } as Request, { send: () => { } } as Response);
+// }, 2000);
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, '../../public')));
